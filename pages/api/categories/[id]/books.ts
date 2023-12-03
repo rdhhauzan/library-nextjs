24c4ec1d2463101ab3ad9c0d1a52eae32488f72c
rdhhauzan <muhammad.hauzan@binus.ac.id>
2023-12-03 12:10:31 +0700

    now show books by category can receive additional filter

--- pages/api/categories/[id]/books.ts
+++ pages/api/categories/[id]/books.ts
@@ -5,24 +5,47 @@ const prisma = new PrismaClient();
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-        const { id } = req.query;
-
-        const data = await prisma.category.findFirst({
-            where: {
-                id: Number(id),
+        const { id, title, minYear, maxYear, minPage, maxPage, sortByTitle } = req.query;
+      
+        const category = await prisma.category.findUnique({
+          where: {
+            id: Number(id),
+          },
+          include: {
+            books: {
+              where: {
+                title: {
+                  contains: title ? String(title) : undefined,
+                  mode: 'insensitive', // Case-insensitive search
+                },
+                release_year: {
+                  gte: minYear ? parseInt(minYear as string) : undefined,
+                  lte: maxYear ? parseInt(maxYear as string) : undefined,
+                },
+                total_page: {
+                  gte: minPage ? parseInt(minPage as string) : undefined,
+                  lte: maxPage ? parseInt(maxPage as string) : undefined,
+                },
+              },
+              orderBy: sortByTitle
+                ? {
+                    title: sortByTitle === 'desc' ? 'desc' : 'asc',
+                  }
+                : undefined,
             },
-            include: {
-                books: true,
-            }
+          },
         });
-
-        if (!data) {
-            return res.status(404).json({ error: "Category not found" });
+      
+        if (!category) {
+          return res.status(404).json({ error: "Category not found" });
         }
-
-        res.status(200).json(data);
-    } catch (error) {
+      
+        // Extract the filtered books from the category
+        const filteredBooks = category.books;
+      
+        res.status(200).json({ ...category, books: filteredBooks });
+      } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal server error" });
-    }
+      }
 };
