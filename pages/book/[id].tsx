42bead600c75025a96d7b990d246230c8b2d052b
rdhhauzan <muhammad.hauzan@binus.ac.id>
2023-12-09 10:50:16 +0700

    Refactor API call to fetch categories in EditBook and AddBook components

--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -71,14 +71,7 @@ export default function EditBook({ id }: { id: string }) {
 
         setLoading(true);
 
-        axios.get('/api/categories')
-        .then((res) => {
-            setCategories(res.data)
-            console.log(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        store.fetchCategories()
 
         store.fetchBookById(Number(router.query.id), () => {
           const { selectedBook } = store;
@@ -216,7 +209,7 @@ export default function EditBook({ id }: { id: string }) {
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               >
                 <option value="" selected disabled>Select Category</option>
-                {categories.map((category) => (
+                {store.categories.map((category) => (
                     <option key={category.id} value={category.id}>{category.name}</option>
                 ))}
               </select>
--- pages/book/index.tsx
+++ pages/book/index.tsx
@@ -69,14 +69,7 @@ const AddBook: React.FC = () => {
       router.push('/login')
     }
 
-    axios.get('/api/categories')
-    .then((res) => {
-      setCategories(res.data)
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    store.fetchCategories();
   }, [])
 
   return (
@@ -194,7 +187,7 @@ const AddBook: React.FC = () => {
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               >
                 <option value="" selected disabled>Select Category</option>
-                {categories.map((category) => (
+                {store.categories.map((category) => (
                     <option key={category.id} value={category.id}>{category.name}</option>
                 ))}
               </select>
